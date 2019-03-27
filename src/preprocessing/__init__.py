8bfd7344f220166b48de4f57778bc0e958e9c694
Pietro Mattia Campi <pietro.campi.dev@gmail.com>
2019-03-27 17:49:10 +0000

    Added DataFrameWrapper as transformer.
    Now you can wrap pieces of pipeline into a pandas DataFrame so that nothing is lost.

--- src/preprocessing/__init__.py
+++ src/preprocessing/__init__.py
@@ -4,3 +4,4 @@ from .base import BasePandasTransformer
 from .selection import ColumnSelector, RowSelector
 from .column_transformers import LogTransformer, MovingAverageTransformer
 from .two_columns_transformers import TwoColumnsTransformer, PercentChangeTransformer
+from .pandas_wrapper import DataFrameWrapper
---
+++ src/preprocessing/pandas_wrapper.py
@@ -0,0 +1,44 @@
+"""Transformer which wraps the specified data into a DataFrame, with the specified columns."""
+
+from typing import List, Tuple, Type, TypeVar, Union, Callable
+
+import numpy as np
+import pandas as pd
+from sklearn.base import BaseEstimator, TransformerMixin
+
+from .base import BasePandasTransformer
+
+
+class DataFrameWrapper(BasePandasTransformer):
+    """Take some data and put it into a DataFrame."""
+
+    def __init__(self, columns: List[str] = None):
+        self._check_init_params(columns)
+        self.columns = columns
+
+    def fit(self, X: np.ndarray, y=None) -> Type['DataFrameWrapper']:
+        if X.ndim == 1:
+            raise ValueError(
+                f"X must have 2 dimensions, got array with shape {X.shape}")
+
+        if not X.shape[1] == len(self.columns):
+            raise ValueError(
+                f"declared columns length and data.shape[1] do not match ([{len(self.columns)}] / [{X.shape[1]}])"
+            )
+
+        return self
+
+    def transform(self, X: np.ndarray):
+        result = pd.DataFrame(data=X, columns=self.columns)
+
+        return result
+
+    def _check_init_params(self, columns: List[str]):
+        if not isinstance(columns, list):
+            raise TypeError(f"columns should be a list, got {type(columns)}")
+
+        if len(columns) == 0:
+            raise ValueError("columns parameter cannot be the empty list")
+
+        for i, column in enumerate(columns):
+            self._check_str(column, f"columns[{i}]")
